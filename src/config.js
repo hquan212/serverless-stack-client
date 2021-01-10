14201483fc7062fe192ca66b00f2de097c8094a5
Henry Quan <henry.quan@liongard.com>
2021-01-09 20:21:34 -0600

    update login

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 const config = {
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-upload-quantisimo212",
+    REGION: "",
+    BUCKET: "",
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://wokd104t5m.execute-api.us-east-1.amazonaws.com/prod",
+    REGION: "",
+    URL: "",
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_nPoc0x4Ak",
-    APP_CLIENT_ID: "7j5jmrlfhk4r8t6vb2r0rkf0uq",
-    IDENTITY_POOL_ID: "us-east-1:3cfc2b9a-bf8f-43aa-bba6-b2ee168a82dd",
+    REGION: "",
+    USER_POOL_ID: "",
+    APP_CLIENT_ID: "",
+    IDENTITY_POOL_ID: "",
   },
 };
 
---
+++ src/containers/Login.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./Login.css";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            placeholder="Enter Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="BasicCheckbox">
+          <Form.Check type="checkbox" label="Check me!" />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+}
