3046ed5c1c5f48f12d925e51874bc536327788c8
Henry Quan <henry.quan@liongard.com>
2021-01-13 11:01:38 -0600

    added new notes page

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,9 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+// import ResetPassword from "./containers/ResetPassword";
 
 export default function Routes() {
   return (
@@ -13,10 +16,19 @@ export default function Routes() {
       <Route exact path="/login">
         <Login />
       </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path="/notes/new">
+        <NewNote />
+      </Route>
       {/* Finally, catch all unmatched routes */}
       <Route>
         <NotFound />
       </Route>
+      {/* <UnauthenticatedRoute exact path="/login/rest">
+        <ResetPassword />
+      </UnauthenticatedRoute> */}
     </Switch>
   );
 }
